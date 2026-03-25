3d6de292a60eb5c80f8a9687ae8143c42d108982
neema-s <shrivastavaneema@gmail.com>
2026-03-25 22:16:34 +0530

    feat: add space rover model

--- src/space_rover/setup.py
+++ src/space_rover/setup.py
@@ -7,12 +7,17 @@ setup(
     version='0.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/rover_sim.launch.py']),
-        ('share/' + package_name + '/urdf', ['urdf/rover.urdf.xacro']),
-    ],
+    ('share/ament_index/resource_index/packages',
+        ['resource/' + package_name]),
+    ('share/' + package_name, ['package.xml']),
+    ('share/' + package_name + '/launch', ['launch/rover_sim.launch.py']),
+    ('share/' + package_name + '/urdf', [
+        'urdf/rover.urdf.xacro',
+        'urdf/rover.gazebo'
+    ]),
+    ('share/' + package_name + '/worlds', ['worlds/mars_terrain.world']),
+],
+
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='neema-s',
